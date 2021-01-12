7c18723f251954d5ca72939376e359f17a04115a
Nathan Holman <nathan.m.holman@gmail.com>
2021-01-12 01:05:52 -0500

    add api call

--- website/app.js
+++ website/app.js
@@ -1,8 +1,31 @@
 /* Global Variables */
-const weatherAPIUrl = 'api.openweathermap.org';
+const weatherAPIUrl = 'http://api.openweathermap.org/data/2.5/weather?';
 const weatherAPIKey = '52e6c7689672cdc242b9dcee26ee3094';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Get data from Weather API
+const getData = async (url, key) => {
+    let zip = document.getElementById('zip').value;
+    let fullURL = `${url}q=${zip}&appid=${key}`
+
+    const request = await fetch(fullURL);
+
+    try {
+        const allData = await request.json()
+        console.log(allData)
+        
+    } catch(error) {
+        console.log("error", error);
+    }
+};
+
+
+function doThing(e) {
+    getData(weatherAPIUrl, weatherAPIKey)
+}
+
+// Click Event
+document.getElementById('generate').addEventListener('click', doThing);
